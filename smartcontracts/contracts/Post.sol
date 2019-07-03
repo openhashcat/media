4328a3b561e2db87b04c9d1899e55704f4b22cca
tiannian <dtiannian@gmail.com>
2019-07-03 13:34:51 +0800

    add contract

---
+++ smartcontracts/contracts/Post.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.24;
+
+interface Post {
+    function getPostProfile(bytes32 _id) external view returns(string);
+    function newPostProfile(string _profile) external;
+}
+
+interface PostEnumerable {
+    function totalPost() external view returns(uint256);
+    function getIndex(uint256 _pos) external view returns(bytes32);
+}
+
+contract MedLinker is Post,PostEnumerable {
+    mapping(bytes32 => string) posts;
+    bytes32[] list;
+    
+    function getPostProfile(bytes32 _id) external view returns(string) {
+        return posts[_id];
+    }
+    
+    function newPostProfile(string _profile) external {
+        bytes32 id = keccak256(abi.encodePacked(_profile));
+        posts[id] = _profile;
+        list.push(id);
+    }
+    
+    function totalPost() external view returns(uint256) {
+        return list.length;
+    }
+    
+    function getIndex(uint256 _pos) external view returns(bytes32) {
+        return list[_pos];
+    }
+}
+
+
