d21eaa7c7b666fcca4c978aed55edf13151209e8
tiannian <dtiannian@gmail.com>
2019-07-06 23:49:41 +0800

    wait check for shaoyaoju

--- medlinker/src/main.js
+++ medlinker/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router';
+
+import Show from './components/Show.vue';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,6 +12,15 @@ Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 
+const router = new VueRouter({
+    routes: [
+        // 动态路径参数 以冒号开头
+        { path: '/', component: Show }
+    ]
+})
+
 new Vue({
-  render: h => h(App),
+    el: '#app',
+    render: h => h(App),
+    router
 }).$mount('#app')
